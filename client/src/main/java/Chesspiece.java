39575a8765b807fb22dbe149585fd612c71514a0
Ty Tanner <Tyjtanner@gmail.com>
2024-01-18 23:07:54 -0700

    changes made to ChessPiece class

--- client/src/main/java/Chesspiece.java
+++ client/src/main/java/Chesspiece.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Chesspiece {
+public class Chesspiece {
+    String type;
+    String color;
+    public Chesspiece(String PieceType, String TeamColor) {
+        this.type = PieceType;
+        this.color = TeamColor;
+    }
+    public String getPieceType(){
+        return type;
+    }
+
+    public String getTeamColor(){
+        return color;
+    }
+
+    public String pieceMoves(){
+        return null;
+    }
 }
