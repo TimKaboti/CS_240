29018b72bbdf14e9863f781c4a23322ba1034a55
Ty Tanner <Tyjtanner@gmail.com>
2024-02-26 10:18:28 -0700

    constructors for gamedata, authdata, and userdata created.

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,4 +1,20 @@
 package model;
 
+import chess.ChessGame;
+
 public class GameData {
+
+    int gameID = 0;
+    String whiteUsername = "";
+    String blackUsername = "";
+    String gameName = "";
+    ChessGame game = new ChessGame();
+
+    public GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+        this.gameID = gameID;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+        this.game = game;
+    }
 }
--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,4 +1,14 @@
 package model;
 
 public class UserData {
+
+    String username = "";
+    String password = "";
+    String email = "";
+
+    public UserData(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
 }
