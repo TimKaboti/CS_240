f6bab30adc58be2fc188537571e453b6a706de75
Ty Tanner <Tyjtanner@gmail.com>
2024-02-26 22:00:21 -0700

    clear,creategame, joingame, listgame, login, logout, and registration service classes created.

---
+++ server/src/main/java/service/RegistrationService.java
@@ -0,0 +1,10 @@
+package service;
+
+import model.AuthData;
+import model.UserData;
+
+public class RegistrationService {
+    public AuthData register(String user) {
+        return null;
+    }
+}
