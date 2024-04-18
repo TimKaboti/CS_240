6117a73f37ac8d23d1f5abc466934b116054d367
tyjta <tyjtanner@gmail.com>
2024-04-18 07:29:25 -0600

    renamed dbproperties back to 'chess'

--- server/src/main/java/service/JoinGameService.java
+++ server/src/main/java/service/JoinGameService.java
@@ -14,8 +14,7 @@ public class JoinGameService {
     public JoinGameResult joinGame(JoinGameRecord record, GameDAO gameMemory, String authToken, AuthDAO authData) throws DataAccessException {
         JoinGameResult joinResult;
         String username;
-        String color =String.valueOf(ChessGame.TeamColor.valueOf(record.playerColor()));
-        String desiredColor= color.toLowerCase();
+//        String color =String.valueOf(ChessGame.TeamColor.valueOf(record.playerColor()));
         try {
             username = authData.getUsername(authToken);
         } catch (DataAccessException e) {
@@ -24,19 +23,13 @@ public class JoinGameService {
 //        if(Objects.equals(username, blackPlayer)){desiredColor = "black";}
 //        if(Objects.equals(username, whitePlayer)){desiredColor = "white";}
         try {
-            String blackPlayer = gameMemory.blackPlayerName(record.gameID());
-            String whitePlayer = gameMemory.whitePlayerName(record.gameID());
+//            String blackPlayer = gameMemory.blackPlayerName(record.gameID());
+//            String whitePlayer = gameMemory.whitePlayerName(record.gameID());
             if (gameMemory.isNotNull(record.gameID())) {
                 if (record.playerColor() == null) {
                     joinResult = new JoinGameResult(gameMemory.getGame(record.gameID()).getBoard(),null);
                 } else if (gameMemory.taken(record.playerColor(), record.gameID())) {
-                    if(desiredColor.equals("black") && Objects.equals(username, blackPlayer)){
-                        gameMemory.joinGame(username, record.playerColor(), record.gameID());
-                    }
-                    else if(desiredColor.equals("white") && Objects.equals(username, whitePlayer)){
-                        gameMemory.joinGame(username, record.playerColor(), record.gameID());
-                    }
-                    else{joinResult=new JoinGameResult(null, "Error: already taken");}
+                    joinResult=new JoinGameResult(null, "Error: already taken");
                 } else {
                     try {
                         gameMemory.joinGame(username, record.playerColor(), record.gameID());
